4fa091abe4694620eae8053af62d3e95ee4438e8
asfopoo <enardo@ycp.edu>
2020-03-01 17:07:17 -0500

    testing fix

--- __tests__/tests.js
+++ __tests__/tests.js
@@ -2,41 +2,34 @@ const app = require('../index.js');
 const supertest = require('supertest');
 const request = supertest(app)
 
-describe('get Endpoints', () => {
-  it('should test that true === true', async done => {
-    expect(true).toBe(true)
-    done()
-  });
+describe('All users', () => {
 
+  it('post to /users', async done => {
 
-  it('post to all users', async done => {
-    const response = await request.get('/users').send({
+    const response = await request.post('/users').send({
+      UUID: process.env.UUID
     });
 
     expect(response.status).toBe(200);
     done()
   });
-});
 
-/*describe('post Endpoint w/o proper credentials', () => {
-  it('post to all users', async done => {
+  it('/users without uuid', async done => {
+
     const response = await request.post('/users').send({
-      UUID: "someBadString"
+    });
+
+    expect(response.status).toBe(401);
+    done()
+  });
+
+  it('/users with wrong uuid', async done => {
+
+    const response = await request.post('/users').send({
+      UUID: 'wrongUUID'
     });
 
     expect(response.status).toBe(403);
     done()
   });
 });
-
-describe('post Endpoint w/o proper params', () => {
-  it('post to approve user', async done => {
-    const response = await request.post('/approveUser').send({
-      UUID: config.UUID,
-      someBadParam: 1
-    });
-
-    expect(response.status).toBe(401);
-    done()
-  });
-});*/
